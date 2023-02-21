b3cd278eda82e14dc82ed36f7f397f5303861f34
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-21 22:02:20 +0300

    Сортировка массива от меньшего к большему

---
+++ Lecture_1.4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Сортировка массива от меньшего к большему
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j; //ищем максимальный элемент
+        }
+        int temporary = array[i]; //временный элемент
+        array[i] = array[minPosition]; // свап
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
