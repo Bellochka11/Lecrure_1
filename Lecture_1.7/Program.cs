e64fbba35dac8b8f2da9c664f04f841a9f1571d6
Белянкина Елизавета <Belyankina@mail.ru>
2023-02-22 15:37:20 +0300

    факториалы

---
+++ Lecture_1.7/Program.cs
@@ -0,0 +1,14 @@
+﻿// факториалы 
+// 1! = 1; 0! = 1
+
+double Factorial(int n) // т.к. тип Int не смещает такие большие значения, как 30! 
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+// int a = Factorial(3); // 3! 1*2*3=6
+// Console.WriteLine(a);
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
